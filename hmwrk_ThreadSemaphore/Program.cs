1b6d53ece43406abef57f742f730913ca560fe0d
GhubUserWq <gogglleacccounty@gmail.com>
2024-12-05 13:30:06 +0500

    minor fixes

--- hmwrk_ThreadSemaphore/Program.cs
+++ hmwrk_ThreadSemaphore/Program.cs
@@ -4,29 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Semaphore semaphoreObject = new Semaphore(initialCount: 3, maximumCount: 3, name: "PrinterApp"); //
-            Printer printerObject = new Printer();
+            Semaphore semaphoreObject = new Semaphore(initialCount: 3, maximumCount: 3, name: "Sinf");  // for class
+            Semaphore semaphorePrinter = new Semaphore(initialCount: 2, maximumCount: 5, name: "Printer");  // for printer
 
-            for (int i = 0; i < 11; ++i)
-            {
-                int j = i;
-                Task.Factory.StartNew(() =>
-                {
-                    semaphoreObject.WaitOne();
-                    printerObject.Print(j);
-                    semaphoreObject.Release();
-                });
-            }
+            ThreadsInClass threadsInClass = new ThreadsInClass();
+            Printer printer = new Printer();
+
+            #region Class
+            //for (short i = 1; i < 11; ++i)
+            //{
+            //    short j = i;
+            //    Task.Factory.StartNew(() =>
+            //    {
+            //        semaphoreObject.WaitOne();
+            //        threadsInClass.Objects(j);
+            //        semaphoreObject.Release();
+            //    });
+            //}
+            #endregion
+
+            #region Printer
+            //for (short i = 1; i < 6; ++i)
+            //{
+            //    short j = i;
+            //    Task.Factory.StartNew(() =>
+            //    {
+            //        semaphorePrinter.WaitOne();
+            //        printer.Users(j);
+            //        semaphorePrinter.Release();
+            //    });
+            //}
+            #endregion
+
             Console.ReadLine();
+        }
+    }
+
+    class ThreadsInClass
+    {
+        public void Objects(short thread)
+        {
+            Console.WriteLine("Incoming: " + thread);
+            Thread.Sleep(TimeSpan.FromSeconds(3));
         }
     }
 
     class Printer
     {
-        public void Print(int documentToPrint)
+        public void Users(short user)
         {
-            Console.WriteLine("Printing document: " + documentToPrint);
-            //code to print document
+            Console.WriteLine($"User " + user + " printing");
             Thread.Sleep(TimeSpan.FromSeconds(3));
         }
     }
