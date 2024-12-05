62d59679de8cd6447ea1390d7f86dc9047e26e6e
GhubUserWq <gogglleacccounty@gmail.com>
2024-12-05 11:39:13 +0500

    Add project files.

---
+++ hmwrk_ThreadSemaphore/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace hmwrk_ThreadSemaphore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaphore semaphoreObject = new Semaphore(initialCount: 3, maximumCount: 3, name: "PrinterApp");
+            Printer printerObject = new Printer();
+
+            for (int i = 0; i < 20; ++i)
+            {
+                int j = i;
+                Task.Factory.StartNew(() =>
+                {
+                    semaphoreObject.WaitOne();
+                    printerObject.Print(j);
+                    semaphoreObject.Release();
+                });
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Printer
+    {
+        public void Print(int documentToPrint)
+        {
+            Console.WriteLine("Printing document: " + documentToPrint);
+            //code to print document
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+    }
+}
